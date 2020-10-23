3ee5e0d4ef17324246c63d0a1b2664e0b7a00106
Isidora <isidora.cvetkov96@gmail.com>
2020-10-24 00:12:19 +0200

    Model

---
+++ MarketStore/MarketStore/Model/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketStore.Model
+{
+    public class Card
+    {
+        public int Turnover { get; set; }
+        public int PurchaseValue { get; set; }
+        public string CardType { get; set; }
+        public double DiscontRate { get; set; }
+        public double Discont { get; set; }
+        public double Total { get; set; }
+    }
+}
